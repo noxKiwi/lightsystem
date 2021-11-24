b3d188126e32f6bb8976a44b5de9c466ab46563f
Jan Nox <jan.nox@pm.me>
2021-11-24 20:26:37 +0100

    Make alarm client beep again

--- resource/js/AlarmClient.js
+++ resource/js/AlarmClient.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var bellSound = new buzz.sound("/asset/lib/sound/alert.mp3");
+
 /**
  * I am the AlarmClient.
  */
@@ -14,7 +16,8 @@ class AlarmClient extends BaseClient
             counter    : -1,    // I am the change counter for the service.
             alarmCount : -1,    // I am the amount of not-acknowledged alarms.
             list       : [],    // I am the list of alarms.
-            server     : ""     // I am the server name.
+            server     : "",    // I am the server name.
+            lastCount  : null   // I am the last count of the getCount method to determine changes.
         };
     }
 
@@ -94,16 +97,22 @@ class AlarmClient extends BaseClient
         await this.doRequest("count").then((data) => {
             this.data.count = data.response;
         });
-        
+
         $(".alarmCountBadge").html(this.data.count);
-        
-        if (this.data.count === 0) {
-            $('.alarmIconDisengaged').removeClass('d-none');
-            $('.alarmIconEngaged').addClass('d-none');
-        } else {
-            $('.alarmIconDisengaged').addClass('d-none');
-            $('.alarmIconEngaged').removeClass('d-none');
+
+        if (this.data.count === this.data.lastCount) {
+            return;
         }
+
+        if (this.data.count === 0) {
+            $(".alarmIconDisengaged").removeClass("d-none");
+            $(".alarmIconEngaged").addClass("d-none");
+        } else {
+            $(".alarmIconDisengaged").addClass("d-none");
+            $(".alarmIconEngaged").removeClass("d-none");
+            bellSound.play();
+        }
+        this.data.lastCount = this.data.count;
         return this.data.count;
     }
 
